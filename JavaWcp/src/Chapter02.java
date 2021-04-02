e3834da887b6fb3aca92d10e9604ace889231c07
masutaniR <popohu87@gmail.com>
2021-04-02 21:56:24 +0900

    [Update] chapter02

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,6 +1,7 @@
 public class Chapter02 {
 	public static void main(String[] args) {
-    // メソッドの処理を記述
-		System.out.println("こんにちは！");
+        // メソッドの処理を記述
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
 	}
 }
