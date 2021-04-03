6adbafa48a65f7b6eb29af9729d551bfb4f66758
masutaniR <popohu87@gmail.com>
2021-04-03 22:48:37 +0900

    [Add] 課題4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int LUCKY_NUM = 8282;
+		String name = "ますたに";
+		System.out.println(name);
+
+		name = "デルマ";
+		System.out.println(name);
+
+		System.out.println(LUCKY_NUM);
+	}
+
+}
