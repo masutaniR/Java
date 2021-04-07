fc421f009dffb8572704f4831135a9c1959d2509
masutaniR <popohu87@gmail.com>
2021-04-07 19:39:10 +0900

    [Finish] java-8

---
+++ JavaWcp/src/Chapter08.java
@@ -0,0 +1,20 @@
+public class Chapter08 {
+    public static void main(String[] args) {
+    	int num = 1;
+    	while (num < 5) {
+    		System.out.println(num + "の二乗は " + num * num);
+    		num++;
+    	}
+    	int[] array = { 1, 2, 3, 4 };
+    	for (int i = 0; i < array.length; i++) {
+    		System.out.println(array[i]);
+    	}
+
+    	for (int number : array) {
+    		if (number % 2 == 0) {
+    			continue;
+    		}
+    		System.out.println(number);
+    	}
+    }
+}
